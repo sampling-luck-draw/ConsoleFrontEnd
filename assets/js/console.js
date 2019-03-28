d143479c7da4f29a41302c2697551e3e0a1f0d47
ThomasAtlantis <1138670081@qq.com>
2019-03-28 20:14:22 +0800

    console interface almost done except bulletcomment settings

--- assets/js/console.js
+++ assets/js/console.js
@@ -1,30 +1,3 @@
-// let ws = new WebSocket('ws://localhost:1923');
-// ws.onopen = function() {
-//     console.log("open");
-//     ws.send("hello");
-// };
-// ws.onmessage = function(evt) {
-//   console.log(evt.data)
-// };
-// ws.onclose = function(evt) {
-//   console.log("WebSocketClosed!");
-// };
-// ws.onerror = function(evt) {
-//   console.log("WebSocketError!");
-// };
-
-// ws.onmessage = function (message) {
-//     console.log(message.data);
-//     let msg = JSON.parse(message.data);
-//     // switch (msg.action) {
-//     //     case 'who-is-lucky-dog':
-//     //         add_luck_dog(msg);
-//     //         break;
-//     //     default:
-//     //         console.log('unknown action:\n' + message);
-//     // }
-// };
-
 let drawing = false;
 let running = false;
 
@@ -61,38 +34,6 @@ function on_activity_btn_click() {
     }
     running = true;
 }
-
-// function on_reset_button_click() {
-//     ws.send(JSON.stringify({
-//         action: 'reset-page'
-//     }));
-// }
-
-
-// msg: JSON Object
-// function add_luck_dog(msg) {
-//     let user_id = msg['content']['uid'];
-//     let content = $("#user_" + user_id).html();
-//     let user_info_template = `<div id="won_${user_id}" class="valign-wrapper">${content}</div>`;
-//     $("#won-list").append('<li class="collection-item">' + user_info_template + '</li>')
-// }
-
-// function get_participant_list() {
-//     $.ajax({
-//         url: 'get-exist-user',
-//         dataType: 'json',
-//         method: 'GET',
-//         success: function (result) {
-//             for (let i in result) {
-//                 let user_id = result[i]['uid'];
-//                 let user_avatar = result[i]['avatar'];
-//                 let user_name = result[i]['nickname'];
-//                 let user_info_template = `<div id="user_${user_id}" class="valign-wrapper"><img class="circle img" src="${user_avatar}" alt="avatar"><span>${user_name}</span></div>`;
-//                 $("#user-list").append('<li class="collection-item">' + user_info_template + '</li>')
-//             }
-//         }
-//     })
-// }
 $.extend({
     csv: function (record, f) {
         record = record.split(/\n/);
@@ -143,21 +84,88 @@ function import_participants() {
     };
     reader.readAsText(file);
 }
-
 var eCalendar = {
     type  : "time",   //模式，time: 带时间选择; date: 不带时间选择;
-    stamp : false,   //是否转成时间戳，默认true;
+    stamp : false,    //是否转成时间戳，默认true;
     offset: [0, 2],   //弹框手动偏移量;
     format: "yyyy年mm月dd日 hh:ii",   //时间格式 默认 yyyy-mm-dd hh:ii;
     skin  : "#1B7690",   //皮肤颜色，默认随机，可选值：0-8,或者直接标注颜色值;
     step  : 10,   //选择时间分钟的精确度;
     callback:function(v,e) {} //回调函数
 };
-
+function del_row(obj) {
+    var tr = obj.parentNode.parentNode;
+    tr.parentNode.removeChild(tr);
+}
+function drag_prize(event) {
+    event.dataTransfer.setData("Text", event.target.value);
+}
+function drop_prize(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("Text");
+    event.target.value = data;
+}
+function allowDrop(event) {
+    event.preventDefault();
+}
+function check_content(obj) {
+    if (obj.value == "") {
+        del_row(obj);
+        return false;
+    }
+    var tr = $(obj).parent().parent();
+    var obj_index = tr.prevAll().length;
+    $(tr.parent()).find("tr").each(function(index, element) {
+        var input = $(this).children("td:first").find("input").val();
+        if (index != obj_index && obj.value == input) {
+            Materialize.toast('名称重复', 1000);
+            obj.focus();
+            return false;
+        }
+    });
+    return true;
+}
+function show_lucky_dog(username, prizename) {
+    $("#lucky-list-body").append("<tr><td>" + username + "</td><td>" + prizename + "</td></tr>");
+}
 $(document).ready(function () {
     $(".btn-draw-action").click(on_draw_btn_click);
     $(".btn-activity-action").click(on_activity_btn_click);
     $("#participants").change(import_participants);
     $("#start-time").ECalendar(eCalendar);
     $("#end-time").ECalendar(eCalendar);
+    $('select').material_select();
+    $('INPUT.minicolors').minicolors();
+    $(".btn-add-prize").click(function() {
+        if ($('#prize-list-body').find("tr").length == 0 || $('#prize-list-body tr:last').find('input').val()) {
+            $("#prize-list-body").append(`<tr>
+            <td><input class="table-input" style="margin-bottom: 0px;" type="text" draggable="true" ondragstart="drag_prize(event)" onblur="check_content(this)"></input></td>
+            <td><div class="del-prize" onclick="del_row(this)"><i class="mdi mdi-close-circle"></i></div></td>
+            </tr>`);
+        }
+        $('#prize-list-body tr:last').find('input').focus();
+    });
+    $(".btn-add-item").click(function() {
+        if ($('#item-list-body').find("tr").length == 0 || $('#item-list-body tr:last').find('input.item-name').val()) {
+            $("#item-list-body").append(`<tr>
+            <td><input class="table-input item-name" style="margin-bottom: 0px;" type="text" onblur="check_content(this)"></input></td>
+            <td><input readonly="readonly" style="margin-bottom: 0px; color: rgb(64, 77, 91);" ondrop="drop_prize(event)" ondragover="allowDrop(event)"></input></td>
+            <td><input class="table-input" style="margin-bottom: 0px;" type="number" min="1"></input></td>
+            <td><div class="del-item" onclick="del_row(this)"><i class="mdi mdi-close-circle"></i></div></td>
+            </tr>`);
+        }
+        $('#item-list-body tr:last').find('input.item-name').focus();
+    });
+    $("#cur-item-input").on('focus', function() {
+        $(this).val("");
+        var items = "";
+        $("#item-list-body tr").each(function() {
+            var text = $(this).children("td:first").find("input").val();
+            if (text) {
+                items += '<option value="' + text + '" />';
+            }
+        });
+        $("#items").html(items);
+        return false;
+    });
 });
