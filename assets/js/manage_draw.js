7d2282b98e6f9cb0b086651a388613a1a3f5ac78
Daleott <1191067632@qq.com>
2019-04-15 20:20:07 +0800

    按钮功能添加

--- assets/js/manage_draw.js
+++ assets/js/manage_draw.js
@@ -62,24 +62,31 @@ function add_black_user() {
     $('#black_users_list_body').append(tr);
 
 }
+var tem=0;
 function add_bullet(a,b,c) {
+    tem++;
         var tr=document.createElement('tr');
         var th=document.createElement('th');
         var td1=document.createElement('td');
         var td2=document.createElement('td');
+        var div=document.createElement('div');
         tr.className+=bullet_list_style[bullet_num%8];
     td2.style.paddingTop=8;
     td2.style.paddingRight=8;
         th.scope="row";
         th.innerText=a;
         td1.innerText=b;
-        td2.innerHTML=c+'<div class="btn-group btn-group-sm right" role="group" aria-label="Basic example" style:"padding">\n' +
-            '        <button type="button" class="btn btn-secondary">屏蔽该弹幕</button>\n' +
-            '        <button type="button" class="btn btn-danger">将该用户加入到黑名单</button>\n' +
+        div.innerText=c;
+        td2.style.display='flex';
+        td2.appendChild(div);
+        td2.innerHTML+='<div class="btn-group btn-group-sm right" role="group" aria-label="Basic example" style:"padding">\n' +
+            '        <button type="button" class="btn btn-secondary" onclick="forbidden_this_draw(this)">屏蔽该弹幕</button>\n' +
+            '        <button type="button" class="btn btn-danger" onclick="forbidden_this_user(this)">将该用户加入到黑名单</button>\n' +
             '        </div>';
         tr.appendChild(th);
         tr.appendChild(td1);
         tr.appendChild(td2);
+        tr.id=tem;
     $('#bullets_list').append(tr);
     bullet_num++;
 }
@@ -88,4 +95,41 @@ for(var i=1;i<=100;i++){
         add_bullet("13213","frog","不要总想着搞什么大新闻");
     else
         add_bullet("13213","frog","安慕安格瑞");
+}
+function forbidden_this_draw(e) {
+    // this.parent.style.display=none;
+    e.disabled=true;
+    var str=e.parentElement.previousSibling.innerText;
+    console.log(str);
+    var par=e.parentElement;
+    var par_presib=e.parentElement.previousSibling;
+    var par_par=e.parentElement.parentElement;
+    var del=document.createElement('strike');
+    del.innerText=str;
+    par_par.removeChild(par_presib);
+    par_par.prepend(del);
+
+
+    console.log(e.parentElement.parentElement.parentElement.id);
+    e.parentElement.parentElement.parentElement.disabled=false;
+    // this.parent.parent.pointer-events=
+}
+
+function forbidden_this_user(e) {
+
+    black_users_list.push(e.parentElement.parentElement.previousSibling.innerText);
+    var tr=document.createElement('tr');
+    var th=document.createElement('th');
+    var td1=document.createElement('td');
+    var td2=document.createElement('td');
+    th.scope="row";
+    th.innerText=black_users_list.length;
+    td1.innerText="--";
+    td2.innerText=e.parentElement.parentElement.previousSibling.innerText;
+    tr.appendChild(th);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    $('#black_users_list_body').append(tr);
+
+    e.disabled=true;
 }
